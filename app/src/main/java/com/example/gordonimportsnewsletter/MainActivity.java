460ca685df032b91073a2f07e798f2e059e00c30
Jacob Madsen <boogath@gmail.com>
2021-12-11 15:35:33 -0700

    Made progress with api

--- app/src/main/java/com/example/gordonimportsnewsletter/MainActivity.java
+++ app/src/main/java/com/example/gordonimportsnewsletter/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.gordonimportsnewsletter;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.support.v7.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson.JacksonFactory;
+import com.google.api.services.sheets.v4.Sheets;
+import com.google.api.services.sheets.v4.model.Spreadsheet;
 import com.google.gson.Gson;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.ArrayList;
 import java.util.List;
+
+
+
+
+
 
 public class MainActivity extends AppCompatActivity {
     public String string = "";
@@ -18,12 +32,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private String apiKey = "1O9wDZGr2eWvkOIHKdQ2EIMjw50KOnuPpojvwj-byKj0";
-    private String sheetUrl = "https://sheets.googleapis.com/v4/spreadsheets/1O9wDZGr2eWvkOIHKdQ2EIMjw50KOnuPpojvwj-byKj0/?fields=sheets.properties.title"
+    private String sheetUrl = "https://sheets.googleapis.com/v4/spreadsheets/1O9wDZGr2eWvkOIHKdQ2EIMjw50KOnuPpojvwj-byKj0/?fields=sheets.properties.title";
 
     private List<Receive> googleSheetData;
-
-
-
 
     public void buttonSend(View view){
         Intent intent = new Intent(this, SendActivity.class);
@@ -45,73 +56,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void getDataFromAPI() {
-        String url = ""
     }
 
 
 }
-// Third attempt
-// Awesome job Eric
-// This is my second attempt to push it.
-
-/*
-
-public class MainActivity extends AppCompatActivity {
-
-
-    private Button temp;
-    private Button wethforc;
-    private TextView tempResults;
-    private ListView forecastResults;
-    private EditText city;
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        temp = (Button) findViewById(R.id.tempbut);
-        wethforc = (Button) findViewById(R.id.weatherForcast);
-        tempResults = (View) findViewById(R.id.tempView);
-        forecastResults = (ListView) findViewById(R.id.displayBox);
-        city = (EditText) findViewById(R.id.cityName);
-    }
-
-    private final String key = "&apiKey=12458339b3d9f04ad5c9a013d4b6463d";
-    private final String baseUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
-    private Gson gson;
-    private List<WeatherForecastSummary> summaries;
-    private HTTPHelper httpHelper;
-
-    void WeatherManager() {
-        gson = new Gson();
-        httpHelper = new HTTPHelper();
-        summaries = new ArrayList<WeatherForecastSummary>();
-    }
-
-    public void updateActivityDisplay(WeatherConditions conditions) {
-        //pull temperature out of conditions
-        Float value = conditions.getMeasurements().get("temp");
-        Toast.makeText(this, "temperature" +  value, Toast.LENGTH_SHORT).show();
-    }
-
-    public void updateForecastListView(WeatherForecast forecast) {
-        ArrayAdapter<WeatherForecastItem> adapter = new ArrayAdapter<WeatherForecastItem>(this, android.R.layout.simple_list_item_1, forecast.getForecastItems());
-        forecastResults.setAdapter(adapter);
-    }
-
-    public void weatherCaller(View view) {
-        String userInputCity = city.getText().toString();
-        WeatherManager weathermanager = new WeatherManager(this, userInputCity, true);
-        weathermanager.start();
-    }
-
-    public void forecastCaller(View view) {
-        String userInputCity = city.getText().toString();
-        WeatherManager weathermanager = new WeatherManager(this, userInputCity, false);
-        weathermanager.start();
-
-    }
-}
- */
--- app/src/main/java/com/example/gordonimportsnewsletter/Receive.java
+++ app/src/main/java/com/example/gordonimportsnewsletter/Receive.java
@@ -2,6 +2,19 @@ package com.example.gordonimportsnewsletter;
 
 import android.app.Activity;
 
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson.JacksonFactory;
+import com.google.api.services.sheets.v4.Sheets;
+import com.google.api.services.sheets.v4.model.Spreadsheet;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Receive implements java.lang.Runnable {
     private String name;
     private String textMessage;
@@ -21,10 +34,16 @@ public class Receive implements java.lang.Runnable {
 
     @Override
     public void run(){
-
+        try {
+            Receive.test();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (GeneralSecurityException e) {
+            e.printStackTrace();
+        }
     }
 
-    public String getApi() {
+    public void getApi() {
 
     }
 
@@ -54,6 +73,44 @@ public class Receive implements java.lang.Runnable {
 
     public void setPhoneNumber(String avatar) {
         this.phoneNumber = phoneNumber;
+    }
+
+    public static void test() throws IOException, GeneralSecurityException {
+        // The spreadsheet to request.
+        String spreadsheetId = "1O9wDZGr2eWvkOIHKdQ2EIMjw50KOnuPpojvwj-byKj0"; // TODO: Update placeholder value.
+
+        // The ranges to retrieve from the spreadsheet.
+        List<String> ranges = new ArrayList<>(); // TODO: Update placeholder value.
+        ranges.add("A2:C3");
+
+        // True if grid data should be returned.
+        // This parameter is ignored if a field mask was set in the request.
+        boolean includeGridData = false; // TODO: Update placeholder value.
+
+        Sheets sheetsService = createSheetsService();
+        Sheets.Spreadsheets.Get request = sheetsService.spreadsheets().get(spreadsheetId);
+        request.setRanges(ranges);
+        request.setIncludeGridData(includeGridData);
+
+        Spreadsheet response = request.execute();
+
+        // TODO: Change code below to process the `response` object:
+        System.out.println(response);
+    }
+
+    public static Sheets createSheetsService() throws IOException, GeneralSecurityException {
+        HttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+        JsonFactory jsonFactory = new JacksonFactory();
+
+        // TODO: Change placeholder below to generate authentication credentials. See
+        // https://developers.google.com/sheets/quickstart/java#step_3_set_up_the_sample
+        //   "https://www.googleapis.com/auth/spreadsheets.readonly"
+        GoogleCredential credential = new GoogleCredential().setAccessToken("AIzaSyC6EprcaT_R8V1jeesJY-vl3HP6bph6jP0");
+
+
+        return new Sheets.Builder(httpTransport, jsonFactory, credential)
+                .setApplicationName("Google-SheetsSample/0.1")
+                .build();
     }
 
 
